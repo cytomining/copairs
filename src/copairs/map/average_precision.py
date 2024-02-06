128ca79f68b2c863736470f9758b5b9b47f087bc
jessica-ewald <1jess.ewald@gmail.com>
2024-02-06 11:51:51 -0500

    add option to randomly sample negative pairs

--- src/copairs/map/average_precision.py
+++ src/copairs/map/average_precision.py
@@ -26,9 +26,8 @@ def build_rank_lists(pos_pairs, neg_pairs, pos_sims, neg_sims):
     paired_ix, counts = np.unique(ix, return_counts=True)
     return paired_ix, rel_k_list, counts
 
-
 def average_precision(
-    meta, feats, pos_sameby, pos_diffby, neg_sameby, neg_diffby, batch_size=20000
+    meta, feats, pos_sameby, pos_diffby, neg_sameby, neg_diffby, batch_size=20000, sample_neg: float = 1
 ) -> pd.DataFrame:
     columns = flatten_str_list(pos_sameby, pos_diffby, neg_sameby, neg_diffby)
     validate_pipeline_input(meta, feats, columns)
@@ -59,6 +58,20 @@ def average_precision(
         dtype=np.dtype((np.int32, 2)),
         count=neg_total,
     )
+
+    # if sample_neg not equal to 1, randomly sample negative pairs
+    if (sample_neg > 1) & (sample_neg < neg_pairs.shape[0]):
+        sampled_rows = np.random.choice(neg_pairs.shape[0], size=sample_neg, replace=False)
+        neg_pairs = neg_pairs[sampled_rows]
+    elif (sample_neg > 1) & (sample_neg >= neg_pairs.shape[0]):
+        raise UnpairedException("'sample_neg' must be less than the number of negative pairs. There are " + str(neg_pairs.shape[0]) + " negative pairs in this analysis.")
+    elif (sample_neg > 0) & (sample_neg < 1):
+        sample_size = round(sample_neg*neg_pairs.shape[0])
+        sampled_rows = np.random.choice(neg_pairs.shape[0], size=sample_size, replace=False)
+        neg_pairs = neg_pairs[sampled_rows]
+    elif sample_neg <= 0:
+        raise UnpairedException("'sample_neg' must be greater than 0.")
+
 
     logger.info("Computing positive similarities...")
     pos_sims = compute.pairwise_cosine(feats, pos_pairs, batch_size)
